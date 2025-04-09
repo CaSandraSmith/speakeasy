9651b82051bc4f64d3dfe5e4fe3015375e708a45
Ca'Sandra Smith <casresm3@gmail.com>
2025-04-09 11:40:33 -0400

    Created passcode page

---
+++ app/(auth)/passcode.tsx
@@ -0,0 +1,145 @@
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  Text,
+  Animated,
+  Easing,
+  TouchableOpacity,
+} from "react-native";
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
+import { useState, useEffect } from "react";
+import { FontAwesome } from "@expo/vector-icons";
+
+type Inputs = {
+  passcode: string;
+};
+
+export default function Passcode() {
+  const [fadeAnim] = useState(new Animated.Value(0));
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1, // Fade in
+      duration: 1000, // 1 second
+      easing: Easing.ease,
+      useNativeDriver: true,
+    }).start();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Enter your referral code</Text>
+      <Animated.View style={[styles.form, { opacity: fadeAnim }]}>
+        <Controller
+          control={control}
+          rules={{
+            required: "Passcode is required",
+          }}
+          render={({ field: { onChange, onBlur, value } }) => (
+            <View style={styles.inputWrapper}>
+              <FontAwesome
+                name="user"
+                size={20}
+                color="white"
+                style={styles.icon}
+              />
+              <TextInput
+                style={styles.input}
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+                placeholder="Passcode"
+                placeholderTextColor="#a9a9a9"
+              />
+            </View>
+          )}
+          name="passcode"
+        />
+        {errors.passcode && (
+          <Text style={styles.error}>{errors.passcode.message}</Text>
+        )}
+
+       
+        <TouchableOpacity
+          onPress={handleSubmit(onSubmit)}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Submit</Text>
+        </TouchableOpacity>
+      </Animated.View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#1a3636", // Dark luxury green background
+    padding: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  header: {
+    fontSize: 50,
+    marginBottom: 30, // Adds space between the header and the form
+    color: "#a27b5b", // Gold color for the header
+    fontWeight: "bold",
+    textAlign: "left",
+  },
+  form: {
+    width: "100%",
+    backgroundColor: "#2c4747", // Darker form background for contrast
+    padding: 20,
+    borderRadius: 10,
+    elevation: 5, // Adds shadow for a luxury effect
+    alignItems: "flex-end",
+  },
+  input: {
+    height: 50,
+    paddingHorizontal: 15,
+    backgroundColor: "#3e5959", // Dark input background
+    color: "white", // White text inside the input
+    borderRadius: 5,
+    fontSize: 16,
+    width: "100%",
+    paddingLeft: 50, // Added padding to ensure the text doesn't overlap the icon
+  },
+  error: {
+    color: "red",
+    marginBottom: 18,
+  },
+  inputWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 15,
+    backgroundColor: "#3e5959",
+    borderRadius: 5,
+    width: "100%",
+    borderWidth: 1,
+    borderColor: "#a27b5b", // Gold border color for inputs
+  },
+  icon: {
+    marginRight: 10,
+    position: "absolute",
+    paddingLeft: 15,
+    fontFamily: "FontAwesome",
+  },
+  button: {
+    width: 150,
+    backgroundColor: "#a27b5b",
+    borderRadius: 10,
+    padding: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 20,
+    textAlign: "center",
+  },
+});
