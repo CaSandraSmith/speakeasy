478e92e733fa8ae5609beeb204decb3acaefe2bb
Ca'Sandra Smith <casresm3@gmail.com>
2025-04-21 21:19:25 -0400

    Updated signup to persist user

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -16,8 +16,6 @@ import Constants from 'expo-constants';
 
 const FLASK_URL = Constants.expoConfig?.extra?.FLASK_URL;
 
-console.log("FLASK_URL", FLASK_URL);
-
 type Inputs = {
   email: string;
   password: string;
