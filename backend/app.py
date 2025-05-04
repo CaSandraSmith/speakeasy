ab3aa44654237068b87bff4c3c0914ee012f4acc
Jimmy Vu <JMV3211@gmail.com>
2025-05-04 15:07:24 -0700

    Add reviews routes

--- backend/app.py
+++ backend/app.py
@@ -6,14 +6,16 @@ from .extensions import app, db
 from extensions import app, db
 from routes.api import api
 from routes.experiences import experiences
+from routes.reviews import reviews_bp
 
 db.init_app(app)
 
 app.register_blueprint(api, url_prefix='/api')
 app.register_blueprint(experiences, url_prefix='/experiences')
+app.register_blueprint(reviews_bp, url_prefix='/reviews')
 
 with app.app_context():
     db.create_all()
-    
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
