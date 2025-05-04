ff8b2ea1470125debcea7c6bc08d48da6f929c87
Ca'Sandra Smith <casresm3@gmail.com>
2025-05-03 22:30:31 -0400

    created a types file for declaration of interfaces

---
+++ app/types.ts
@@ -0,0 +1,11 @@
+export interface Experience {
+  id: string;
+  title: string;
+  description: string;
+  price: number;
+  location: string;
+  schedule?: [];
+  images?: [];
+  reviews?: [];
+  tags?: [];
+}
